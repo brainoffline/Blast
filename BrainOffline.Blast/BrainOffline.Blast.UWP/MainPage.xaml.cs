79f2dffd352f76963bb3f147b5d80350422e89b1
Brian Donovan-Smith <brian.donovansmith@gmail.com>
2017-11-04 09:12:57 +1300

    Add project files.

---
+++ BrainOffline.Blast/BrainOffline.Blast.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BrainOffline.Blast.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new BrainOffline.Blast.App());
+        }
+    }
+}
---
+++ BrainOffline.Blast/BrainOffline.Blast/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace BrainOffline.Blast
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
